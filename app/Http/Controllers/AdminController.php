651ee88395318fc7b2ac44de1c9c4106cb95ba7d
lahcen idir <128367702+idir2023@users.noreply.github.com>
2025-04-01 12:41:01 +0200

    dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Expertise;
+use App\Models\Actualite;
+use App\Models\Pack;
+use App\Models\Consultation;
+use App\Models\Contact;
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    public function dashboard() {
-        return view('admins.index');
+    public function dashboard()
+    {
+     
+        return view('admins.index', [
+            'totalExpertise' => Expertise::count(),
+            'totalActualite' => Actualite::count(),
+            'totalPack' => Pack::count(),
+            'totalConsultation' => Consultation::count(),
+            'totalContact' => Contact::count(),
+            'totalUser' => User::where('is_admin', false)->count()
+        ]);
     }
 }
--- resources/views/admins/index.blade.php
+++ resources/views/admins/index.blade.php
@@ -1,98 +1,74 @@
 @extends('admins.layouts.master')
 
 @section('content')
-<div class="row">
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-cyan text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
-                <h6 class="text-white">Dashboard</h6>
+    <div class="row">
+        <!-- Column -->
+        <div class="col-md-6 col-lg-2 col-xlg-3">
+            <div class="card card-hover">
+                <div class="box bg-cyan text-center">
+                    <h1 class="font-light text-white"><i class="mdi mdi-account-multiple"></i></h1>
+                    <h6 class="text-white">Users</h6>
+                    <div class="text-white">{{ $totalUser ?? 0 }} users</div>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-4 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-success text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-chart-areaspline"></i></h1>
-                <h6 class="text-white">Charts</h6>
+
+        <!-- Column -->
+        <div class="col-md-6 col-lg-4 col-xlg-3">
+            <div class="card card-hover">
+                <div class="box bg-danger text-center">
+                    <h1 class="font-light text-white"><i class="mdi mdi-message"></i></h1>
+                    <h6 class="text-white">Contacts</h6>
+                    <div class="text-white">{{ $totalContact ?? 0 }} contacts</div>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-warning text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-collage"></i></h1>
-                <h6 class="text-white">Widgets</h6>
+
+        <!-- Column -->
+        <div class="col-md-6 col-lg-2 col-xlg-3">
+            <div class="card card-hover">
+                <div class="box bg-success text-center">
+                    <h1 class="font-light text-white"><i class="mdi mdi-calendar-check"></i></h1>
+                    <h6 class="text-white">Consultations</h6>
+                    <div class="text-white">{{ $totalConsultation ?? 0 }} consultations</div>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-danger text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-border-outside"></i></h1>
-                <h6 class="text-white">Tables</h6>
+
+        <!-- Column -->
+        <div class="col-md-6 col-lg-4">
+            <div class="card card-hover">
+                <div class="box bg-purple text-center">
+                    <h1 class="font-light text-white"><i class="mdi mdi-certificate"></i></h1>
+                    <h6 class="text-white">Expertises</h6>
+                    <div class="text-white">{{ $totalExpertise ?? 0 }} total</div>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-info text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-arrow-all"></i></h1>
-                <h6 class="text-white">Full Width</h6>
+
+        <!-- Column -->
+        <div class="col-md-6 col-lg-4">
+            <div class="card card-hover">
+                <div class="box bg-primary text-center">
+                    <h1 class="font-light text-white"><i class="mdi mdi-newspaper"></i></h1>
+                    <h6 class="text-white">Actualités</h6>
+                    <div class="text-white">{{ $totalActualite ?? 0 }} active</div>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- Column -->
-    <!-- Column -->
-    <div class="col-md-6 col-lg-4 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-danger text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-receipt"></i></h1>
-                <h6 class="text-white">Forms</h6>
+
+        <!-- Column -->
+        <div class="col-md-6 col-lg-4">
+            <div class="card card-hover">
+                <div class="box bg-secondary text-center">
+                    <h1 class="font-light text-white"><i class="mdi mdi-package"></i></h1>
+                    <h6 class="text-white">Packs</h6>
+                    <div class="text-white">{{ $totalPack ?? 0 }} available</div>
+                </div>
             </div>
         </div>
+    
+
     </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-info text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-relative-scale"></i></h1>
-                <h6 class="text-white">Buttons</h6>
-            </div>
-        </div>
-    </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-cyan text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-pencil"></i></h1>
-                <h6 class="text-white">Elements</h6>
-            </div>
-        </div>
-    </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-success text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-calendar-check"></i></h1>
-                <h6 class="text-white">Calnedar</h6>
-            </div>
-        </div>
-    </div>
-    <!-- Column -->
-    <div class="col-md-6 col-lg-2 col-xlg-3">
-        <div class="card card-hover">
-            <div class="box bg-warning text-center">
-                <h1 class="font-light text-white"><i class="mdi mdi-alert"></i></h1>
-                <h6 class="text-white">Errors</h6>
-            </div>
-        </div>
-    </div>
-    <!-- Column -->
-</div>
 @endsection
+ 
